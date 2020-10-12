d2e7bf0497c0b80a224d0255959136d9cd28b5b2
Aaron Lu <aaronnoh0409@gmail.com>
2020-10-11 21:52:51 -0400

    idk what i did but i spent about 3 hrs to find a bug

--- assets/quiz.js
+++ assets/quiz.js
@@ -1,16 +1,15 @@
 //Dom Elements 
 const question = document.querySelector('#question');
-const choices = Array.from(document.querySelector('.choice-text'));
+const choices = Array.from(document.querySelectorAll('.choice-text'));
 const progressText = document.querySelector('#progressText');
-const score = document.querySelector('#score');
+const scoreText = document.querySelector('#score');
 const progressBarFull = document.querySelector('#progressBarFull');
 
-
-let currentQuestion = {};
-let acceptingAnswers = true;
-let score = 0;
-let questionCounter = 0;
-let availableQuestions = [];
+let currentQuestion = {}
+let acceptingAnswers = true
+let score = 0
+let questionCounter = 0
+let availableQuestions = []
 
 let questions = [
     {
@@ -35,7 +34,7 @@ let questions = [
         choice2: '2',
         choice3: '3',
         choice4: '4',
-        answer: 2,
+        answer: 3,
     },
     {
         question: 'what is 1+1',
@@ -43,56 +42,80 @@ let questions = [
         choice2: '2',
         choice3: '3',
         choice4: '4',
-        answer: 2,
-    },
+        answer: 4,
+    }
     
 ];
 
 //Fixed 
-const SCORE_POINTS = 100;
-const MAX_QUESTIONS = 7;
+const SCORE_POINTS = 100
+const MAX_QUESTIONS = 7
 
 //Getting questions values  
 startQuiz = () => {
-    questionCounter = 0;
-    score = 0;
-    availableQuestions = [...questions];
-    getNewQuestion();
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    getNewQuestion()
 }
 
 
-//Functions 
-getNewQuestion = () => {
-
 //Tracking the score
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS){
-        localStorage.setItem('mostRecentScore', score);
+getNewQuestion = () => {
+    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
+        localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/end.html');
+        return window.location.assign('end.html')
     }
 
 //keeping on track of the questions
     questionCounter++
-    progressText.innerText = `question ${questionCounter} of ${MAX_QUESTIONS}`;
-    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`;
+    progressText.innerText = `Question ${questionCounter} of ${MAX_QUESTIONS}`
+    progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
 
 //Asking on track question 
-    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
-    currentQuestion = availableQuestions[questionIndex];
-    question.innerText = currentQuestion.question;
+    const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
+    currentQuestion = availableQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
 
 //Takes the choice that user picked
     choices.forEach(choice => {
-        const number = choice.dataset ['number'];
-        choice.innerText = currentQuestion['choice' + number];
+        const number = choice.dataset ['number']
+        choice.innerText = currentQuestion['choice' + number]
     })
 
-    availableQuestions.splice(questionIndex, 1);
+    availableQuestions.splice(questionsIndex, 1)
 
-    acceptingAnswers = true;
+    acceptingAnswers = true
 }
 
+//Function for answer Choices 
 choices.forEach(choice => {
-    choice.addEventListener('click', e=> {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+    //If the answer is correct +100 points
+        if(classToApply === 'correct') {
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        }, 1000)
     })
 })
+
+incrementScore = num => {
+    score += num
+    scoreText.innerText = score
+}
+
+startQuiz()
