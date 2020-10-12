e60470f59d8f0c255eddeff1db8e86a497f5e73e
Aaron Lu <aaronnoh0409@gmail.com>
2020-10-11 22:02:50 -0400

    created another page called result to log the scores

--- assets/quiz.js
+++ assets/quiz.js
@@ -65,7 +65,7 @@ getNewQuestion = () => {
     if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('end.html')
+        return window.location.assign('result.html')
     }
 
 //keeping on track of the questions
